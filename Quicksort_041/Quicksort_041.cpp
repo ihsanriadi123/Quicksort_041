59ace48d1aa87f6e7ce0eb330193134dcf549ff8
ihsanriadi123 <iputrariadi@gmail.com>
2023-03-30 11:45:37 +0700

    menampilakn void display nya

--- Quicksort_041/Quicksort_041.cpp
+++ Quicksort_041/Quicksort_041.cpp
@@ -96,3 +96,17 @@ void q_short(int low, int high)
 
 }
 
+
+void display() {
+	cout << "\n==================" << endl;
+	cout << "sorted array" << endl;
+	cout << "==================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nnumber of comparasions; " << cmp_cout << endl;
+	cout << "number of data movements: " << mov_cout << endl;
+}
