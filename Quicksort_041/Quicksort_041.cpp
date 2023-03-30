ed279527740a3de0c81a298e0987eeb3b2a30268
ihsanriadi123 <iputrariadi@gmail.com>
2023-03-30 10:57:18 +0700

    memasukkan panjang element array

--- Quicksort_041/Quicksort_041.cpp
+++ Quicksort_041/Quicksort_041.cpp
@@ -7,3 +7,25 @@ int cmp_cout = 0; //number of comparasion
 int mov_cout = 0; //number of data movements
 int n;
 
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nmaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n==================" << endl;
+	cout << "\nenter array element" << endl;
+	cout << "\n==================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
